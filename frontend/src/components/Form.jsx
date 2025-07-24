6e98d4e6ee6e340316a530af60c517b3aec0df14
Jabbar Baloghlan <103314329+Jobi060704@users.noreply.github.com>
2025-07-24 13:35:56 +0400

    fix (res data naming mixup)

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -22,7 +22,7 @@ function Form({route, method}) {
             const res = await api.post(route, {username, password});
 
             if(method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.token);
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
 
                 navigate("/");
