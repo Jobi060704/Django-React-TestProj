7d3de7b69da390d25ee1e57e3e1a905300e1d8ca
Jabbar Baloghlan <103314329+Jobi060704@users.noreply.github.com>
2025-07-25 16:53:01 +0400

    email enforcement

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,11 +3,18 @@ from rest_framework import serializers
 from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-    
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}, "email": {"required": True}}
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
