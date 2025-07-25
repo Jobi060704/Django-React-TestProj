408cd68fdfe407fa03c690ed0a82d6b4becb045e
Jabbar Baloghlan <103314329+Jobi060704@users.noreply.github.com>
2025-07-25 16:44:01 +0400

    .env change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "ENGINE": os.getenv("DB_ENGINE"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PWD"),
