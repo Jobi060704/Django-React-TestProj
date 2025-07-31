a107a64bad39a6766dd8ed26f8fcbbfcd2541f79
Jabbar Baloghlan <103314329+Jobi060704@users.noreply.github.com>
2025-07-31 17:07:43 +0400

    sidebar change 2

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,87 +1,109 @@
-// src/components/Sidebar.jsx
+import {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-import {FaChartBar,FaIndustry,FaMapMarkedAlt,FaDrawPolygon,FaTint,FaSolarPanel,FaLandmark,FaTable} from "react-icons/fa";
-import {FaCircleDot, FaCircleH, FaMapLocation, FaPersonHiking, FaTableCells, FaTruckField} from "react-icons/fa6";
+import {
+    FaChartBar, FaIndustry, FaMapMarkedAlt, FaDrawPolygon,
+    FaTint, FaSolarPanel, FaLandmark, FaTable
+} from "react-icons/fa";
+import {
+    FaCircleDot, FaCircleH, FaMapLocation, FaPersonHiking,
+    FaTableCells, FaTruckField, FaChevronLeft, FaChevronRight
+} from "react-icons/fa6";
 import "../styles/Sidebar.css";
 
 const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(() => {
+        const stored = localStorage.getItem("sidebarCollapsed");
+        return stored === "true"; // convert from string to boolean
+    });
+
+    // Store new state on change
+    useEffect(() => {
+        localStorage.setItem("sidebarCollapsed", collapsed);
+    }, [collapsed]);
+
     return (
-        <aside className="sidebar">
-            <ul> {/*Landing*/}
-                <li>
-                    <Link to="/dashboard">
-                        <FaLandmark className="sidebar-icon" />
-                        Landing
-                    </Link>
-                </li>
-            </ul>
+        <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/dashboard">
+                            <FaLandmark className="sidebar-icon" />
+                            <span>Landing</span>
+                        </Link>
+                    </li>
+                </ul>
 
-            <ul> {/*Dashboard and Map*/}
-                <li>
-                    <Link to="/dashboard/statistics">
-                        <FaChartBar className="sidebar-icon" />
-                        Statistics
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/map">
-                        <FaMapLocation className="sidebar-icon" />
-                        Map view
-                    </Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <Link to="/dashboard/statistics">
+                            <FaChartBar className="sidebar-icon" />
+                            <span>Statistics</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/map">
+                            <FaMapLocation className="sidebar-icon" />
+                            <span>Map view</span>
+                        </Link>
+                    </li>
+                </ul>
 
-            <ul> {/*Models*/}
-                <li>
-                    <Link to="/dashboard/companies">
-                        <FaIndustry className="sidebar-icon" />
-                        Companies
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/regions">
-                        <FaMapMarkedAlt className="sidebar-icon" />
-                        Regions
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/sectors">
-                        <FaDrawPolygon className="sidebar-icon" />
-                        Sectors
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/pivots">
-                        <FaCircleDot className="sidebar-icon" />
-                        Pivots
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/fields">
-                        <FaSolarPanel className="sidebar-icon" />
-                        Fields
-                    </Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <Link to="/dashboard/companies">
+                            <FaIndustry className="sidebar-icon" />
+                            <span>Companies</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/regions">
+                            <FaMapMarkedAlt className="sidebar-icon" />
+                            <span>Regions</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/sectors">
+                            <FaDrawPolygon className="sidebar-icon" />
+                            <span>Sectors</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/pivots">
+                            <FaCircleDot className="sidebar-icon" />
+                            <span>Pivots</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/fields">
+                            <FaSolarPanel className="sidebar-icon" />
+                            <span>Fields</span>
+                        </Link>
+                    </li>
+                </ul>
 
-            <ul> {/*Extras*/}
-                <li>
-                    <Link to="/scouting">
-                        <FaPersonHiking className="sidebar-icon" />
-                        Scouting
-                    </Link>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <Link to="/scouting">
+                            <FaPersonHiking className="sidebar-icon" />
+                            <span>Scouting</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/croprotation">
+                            <FaTable className="sidebar-icon" />
+                            <span>Crop Rotation</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
-            <ul> {/*Extras*/}
-                <li>
-                    <Link to="/croprotation">
-                        <FaTable className="sidebar-icon" />
-                        Crop Rotation
-                    </Link>
-                </li>
-            </ul>
-
+            <button
+                className="toggle-button"
+                onClick={() => setCollapsed(prev => !prev)}
+                aria-label="Toggle Sidebar"
+            >
+                {collapsed ? <FaChevronRight /> : <FaChevronLeft />}
+            </button>
         </aside>
     );
 };
